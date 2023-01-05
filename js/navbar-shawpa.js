0d216b1b8bf71540d47ad6ecc7030f495b4a2154
dknyz <dknyzzz777@gmail.com>
2023-01-05 16:38:56 +0800

    Refactor : Change the dropdown-list show logic in navbar-shawpa.js.

--- js/navbar-shawpa.js
+++ js/navbar-shawpa.js
@@ -2,26 +2,22 @@ const chooseRegion = document.querySelector(".choose-icon");
 const mainLeft = document.querySelector(".main-left");
 
 chooseRegion.addEventListener('click',(e) => {    
-    console.log(e.target.classList.value)
     if(e.target.classList.contains("fa-angles-down")){
         mainLeft.style.display = "block";
         e.target.classList.remove("fa-angles-down");
         e.target.classList.add("fa-angles-up");
-        mainLeft.addEventListener("click", function(){
-            this.classList.add("main-left-collapse");
-            setTimeout(() => {
-                console.log(e.target.classList.value)
-                this.classList.remove("main-left-collapse");;
-                e.target.classList.remove("fa-angles-up");
-                e.target.classList.add("fa-angles-down");        
-                mainLeft.style.display = "none";
+    }else{
+        e.target.classList.add("fa-angles-down");
+        e.target.classList.remove("fa-angles-up");
+
+        mainLeft.classList.add("main-left-collapse");
+        setTimeout(() => {
+            mainLeft.classList.remove("main-left-collapse");;
+            e.target.classList.remove("fa-angles-up");
+            e.target.classList.add("fa-angles-down");        
+            mainLeft.style.display = "none";
             },
             1500
-            );
-        })               
-    }else{
-        mainLeft.style.display = "none";
-        e.target.classList.add("fa-angles-down");
-        e.target.classList.remove("fa-angles-up")    
+        )
     }
 })
